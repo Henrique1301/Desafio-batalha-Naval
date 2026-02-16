f5b52e61f8f2e9b79414fb1d7b4fe0afb865f6b3
Henrique1301 <73301553+Henrique1301@users.noreply.github.com>
2026-02-16 19:54:03 +0000

    atualização nivel intermediario

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,26 +1,55 @@
 #include <stdio.h>
 
+#define Linhas 10
+#define Colunas  10
+
+int navio1 = 0;
+
 int main(){
 
-    int tabuleiro [10][10] = {
-    {0,0,0,3,3,3,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,3,0,0,0,0},
-    {0,0,0,0,0,3,0,0,0,0},
-    {0,0,0,0,0,3,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0}
-    };
+    int tabuleiro [Linhas][Colunas]; 
 
-
-    printf("  A B C D E F G H I J\n");
-    for (int i = 0; i < 10; i++)
+    int soma = 0;
+    
+    printf("   A B C D E F G H I J\n");
+    for (int i = 0; i < Linhas; i++)
     {
-        printf("%d %d %d %d %d %d %d %d %d %d %d\n", i, tabuleiro[i][0], tabuleiro[i][1], tabuleiro[i][2], tabuleiro[i][3], tabuleiro[i][4], tabuleiro[i][5], tabuleiro[i][6], tabuleiro[i][7], tabuleiro[i][8], tabuleiro[i][9]);
+        printf("%d  ",i+1);
+        for (int j = 0; j < Colunas; j++)
+        {
+            soma = 0;
+            int navio = 3;
+            tabuleiro [Linhas][Colunas]= soma;
+            
+            //Definindo navios horizontal
+            if (i == 5 && j == 2 || i == 6 && j == 2 || i == 7 && j == 2)
+            {
+                tabuleiro [Linhas][Colunas] = navio;
+            }
+
+            //definindo navio vertical
+            if (i == 0 && j == 3 || i == 0 && j == 4 || i == 0 && j == 5)
+            {
+                tabuleiro [Linhas][Colunas] = navio;
+            }
+
+            //definindo navio horizontal (esquerda para direita)
+             if (i == 5 && j == 5 || i == 6 && j == 6 || i == 7 && j == 7)
+            {
+                tabuleiro [Linhas][Colunas] = navio;
+            }
+
+            //definindo navio horizontal (direita para esquerda)
+             if (i == 0 && j == 9 || i == 1 && j == 8 || i == 2 && j == 7)
+            {
+                tabuleiro [Linhas][Colunas] = navio;
+            }
+            
+            printf("%d ", tabuleiro[Linhas][Colunas]);
+        }
+        printf("\n");
     }
+
 
 
     return 0; 
